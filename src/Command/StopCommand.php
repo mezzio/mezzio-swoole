67b7be1a4d9d9bc5a2df99350bfcb4e361a1bab8
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:47:22 -0600

    Rewriting as Laminas Project package

--- src/Command/StopCommand.php
+++ src/Command/StopCommand.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-swoole for the canonical source repository
- * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-swoole/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-swoole for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-swoole/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-swoole/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace Zend\Expressive\Swoole\Command;
+namespace Mezzio\Swoole\Command;
 
+use Mezzio\Swoole\SwooleRequestHandlerRunner;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Zend\Expressive\Swoole\SwooleRequestHandlerRunner;
 
 class StopCommand extends Command
 {
--- src/StaticResourceHandler/ValidateRegexTrait.php
+++ src/StaticResourceHandler/ValidateRegexTrait.php
@@ -1,15 +1,16 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-swoole for the canonical source repository
- * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-swoole/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-swoole for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-swoole/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-swoole/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace Zend\Expressive\Swoole\StaticResourceHandler;
+namespace Mezzio\Swoole\StaticResourceHandler;
 
-use Zend\Expressive\Swoole\Exception;
+use Mezzio\Swoole\Exception;
 
 use function preg_match;
 use function restore_error_handler;
