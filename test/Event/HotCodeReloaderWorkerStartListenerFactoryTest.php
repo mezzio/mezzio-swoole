5b974a8d5da5a2e6dee21ef3ef53dbb39003f9b2
Matthew Weier O'Phinney <matthew@weierophinney.net>
2020-09-24 09:30:50 -0500

    qa: adds tests for HotCodeReloaderWorkerStartListenerFactory
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

---
+++ test/Event/HotCodeReloaderWorkerStartListenerFactoryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @see       https://github.com/mezzio/mezzio-swoole for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-swoole/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-swoole/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace MezzioTest\Swoole\Event;
+
+use Mezzio\Swoole\Event\HotCodeReloaderWorkerStartListener;
+use Mezzio\Swoole\Event\HotCodeReloaderWorkerStartListenerFactory;
+use Mezzio\Swoole\HotCodeReload\Reloader;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+class HotCodeReloaderWorkerStartListenerFactoryTest extends TestCase
+{
+    public function testProducesHotCodeReloaderListenerUsingReloaderFromContainer(): void
+    {
+        $reloader = $this->createMock(Reloader::class);
+        $container = $this->createMock(ContainerInterface::class);
+        $container
+            ->expects($this->once())
+            ->method('get')
+            ->with(Reloader::class)
+            ->willReturn($reloader);
+
+        $factory  = new HotCodeReloaderWorkerStartListenerFactory();
+        $listener = $factory($container);
+
+        $this->assertInstanceOf(HotCodeReloaderWorkerStartListener::class, $listener);
+    }
+}
