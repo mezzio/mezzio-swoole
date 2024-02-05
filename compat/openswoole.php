faa15c1f5e20a6cae59191826781dd72e1b12552
Slavey Karadzhov <slav@attachix.com>
2024-02-05 14:46:31 +0100

    Prepare for working with newer versions of OpenSwoole.
    Signed-off-by: Slavey Karadzhov <skaradzhov@perforce.com>

---
+++ compat/openswoole.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use OpenSwoole\Util;
+
+// @see https://github.com/mezzio/mezzio-swoole/issues/110#issuecomment-1500174967
+// Override the swoole_set_process_name function
+if (version_compare(phpversion('openswoole'), '22.0.0', '>=')) {
+    function swoole_set_process_name(string $process_name): void
+    {
+        Util::setProcessName($process_name);
+    }
+}
