3df9c7dc7988068807628ebdb43d5ca365090331
mmalac <mmalac@sygic.com>
2025-10-11 16:28:22 +0200

    feat: revert `psr/http-message-implementation` dependency
    Signed-off-by: mmalac <mmalac@sygic.com>

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,9 +3,12 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 $config = new Configuration();
 return $config
     // Ignore unknown classes for PHP 8.2 compatibility. Remove after dropping PHP 8.2 support.
-     ->ignoreUnknownClasses([Override::class])
-    ->ignoreUnknownFunctions(["inotify_add_watch", "inotify_init", "inotify_read"]);
+    ->ignoreUnknownClasses([Override::class])
+    ->ignoreUnknownFunctions(["inotify_add_watch", "inotify_init", "inotify_read"])
+    //meta-package with no code; only declares requirement for PSR-7 implementation
+    ->ignoreErrorsOnPackage('psr/http-message-implementation', [ErrorType::UNUSED_DEPENDENCY]);
