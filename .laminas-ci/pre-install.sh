537084d2609952b8d15420566a8e845352679fbc
Aleksei Khudiakov <aleksey@xerkus.pro>
2026-04-12 12:33:50 +1000

    Move inotify back to .laminas-ci.json
    Signed-off-by: Aleksei Khudiakov <aleksey@xerkus.pro>

--- .laminas-ci/pre-install.sh
+++ .laminas-ci/pre-install.sh
@@ -7,9 +7,6 @@ apt update
 
 # Build package list
 PACKAGES="php${PHP_VERSION}-swoole"
-if [ "$PHP_VERSION" != "8.4" ]; then
-    PACKAGES="$PACKAGES php${PHP_VERSION}-inotify"
-fi
 
 # Install all packages at once
 apt install -y $PACKAGES
